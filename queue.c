16c33703e7f59c2f3704458ac374be1c7ef845b5
korin <k777k777tw@gmail.com>
2023-02-19 21:34:31 +0800

    Implement q_remove_head
    Remove an element from head and copy
    the element's value to sp if it isn't
    NULL.

--- queue.c
+++ queue.c
@@ -61,7 +61,17 @@ bool q_insert_tail(struct list_head *head, char *s)
 /* Remove an element from head of queue */
 element_t *q_remove_head(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    if (!head || list_empty(head))
+        return NULL;
+    element_t *ele = list_first_entry(head, element_t, list);
+    list_del(&ele->list);
+    if (sp) {
+        size_t n = strlen(ele->value) + 1;
+        n = bufsize < n ? bufsize : n;
+        strncpy(sp, ele->value, n);
+        sp[n - 1] = '\0';
+    }
+    return ele;
 }
 
 /* Remove an element from tail of queue */
